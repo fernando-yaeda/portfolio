13ce327d0fdd5be1a37e5f36c51d653ae8fb5416
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-06-09 16:39:34 -0300

    feat: create button and icon components

---
+++ src/components/icons.tsx
@@ -0,0 +1,9 @@
+import { Github, Linkedin, Mail, type Icon as LucideIcon } from "lucide-react"
+
+export type Icon = LucideIcon
+
+export const Icons = {
+  github: Github,
+  linkedin: Linkedin,
+  mail: Mail,
+}
