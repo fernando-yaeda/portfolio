504626e0a655140402eebf82416e8ad4c0546e2e
fernando yaeda <fernandoyudi.yaeda@gmail.com>
2023-06-09 22:58:59 -0300

    feat: add tech icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -8,6 +8,14 @@ import {
   Sun,
   type Icon as LucideIcon,
 } from "lucide-react"
+import { IconType as ReactIcon } from "react-icons"
+import { DiNodejsSmall, DiReact } from "react-icons/di"
+import {
+  SiNestjs,
+  SiNextdotjs,
+  SiTailwindcss,
+  SiTypescript,
+} from "react-icons/si"
 
 export type Icon = LucideIcon
 
@@ -19,4 +27,15 @@ export const Icons = {
   sun: Sun,
   moon: Moon,
   laptop: Laptop,
+}
+
+export type TechIcon = ReactIcon
+
+export const TechIcons = {
+  typescript: SiTypescript,
+  next: SiNextdotjs,
+  nest: SiNestjs,
+  react: DiReact,
+  node: DiNodejsSmall,
+  tailwind: SiTailwindcss,
 }
